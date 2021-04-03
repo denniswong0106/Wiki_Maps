506c808d3948ab7116fa3d33cc069189bcb54406
Dennis Wong <denniswong0106@gmail.com>
2021-04-03 17:36:47 +0000

    fixed small issue with favorite TABLE given wrong name

--- db/schema/03_favorites.sql
+++ db/schema/03_favorites.sql
@@ -2,8 +2,8 @@ DROP TABLE IF EXISTS favorites CASCADE;
 
 --- Table for favorites---
 -- Links Users and Maps in a Many to Many relationship
-CREATE TABLE maps (
+CREATE TABLE favorites (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  map_id INTEGER REFERENCES map(id) ON DELETE CASCADE
+  map_id INTEGER REFERENCES maps(id) ON DELETE CASCADE
 );
--- db/schema/05_widgets.sql
+++ db/schema/05_widgets.sql
@@ -2,8 +2,8 @@
 -- NOT USED ---- KEEP FOR NOW AS SKELETON
 
 DROP TABLE IF EXISTS widgets CASCADE;
-CREATE TABLE widgets (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
-  name VARCHAR(255) NOT NULL
-);
+-- CREATE TABLE widgets (
+--   id SERIAL PRIMARY KEY NOT NULL,
+--   user_id INTEGER REFERENCES users(id),
+--   name VARCHAR(255) NOT NULL
+-- );
