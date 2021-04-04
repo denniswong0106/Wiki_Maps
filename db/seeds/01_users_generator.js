aee63292585899f6b66eafcb63aaf022522b697d
Dennis Wong <denniswong0106@gmail.com>
2021-04-04 16:52:32 +0000

    managed to use knex to generate a bunch of fake user data. Needed to use psql superuser privileges to grant user 'labber' access to data tables

--- db/seeds/01_users_generator.js
+++ db/seeds/01_users_generator.js
@@ -1,10 +1,8 @@
 const faker = require('faker');
 
-const desiredFakeUsers = process.argv[3] || 20000;
-const batchSize = 50000;
 
 const createFakeUser = () => ({
-  first_name: faker.name.firstName() + faker.name.lastName(),
+  name: faker.name.firstName() + ' ' + faker.name.lastName(),
   email: faker.internet.email(),
   city: faker.address.city(),
   profile_pic: faker.image.avatar()
@@ -12,12 +10,14 @@ const createFakeUser = () => ({
 
 exports.seed = async (knex) => {
   let fakeUsers = [];
+  const desiredFakeUsers = 1000;
   for (let i = 0; i < desiredFakeUsers; i++) {
     fakeUsers.push(createFakeUser());
-    if (fakeUsers.length >= batchSize) {
-      await knex('users').insert(fakeUsers);
-      fakeUsers = [];
-    }
   }
-  return knex('users').insert(fakeUsers);
+  await knex('users').insert(fakeUsers);
 };
+
+
+
+//   return knex('users').insert(fakeUsers);
+// };
