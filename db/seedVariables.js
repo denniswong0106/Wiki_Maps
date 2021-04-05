963e825342579f9b6da3e2f2d3ac382903126f1c
Dennis Wong <denniswong0106@gmail.com>
2021-04-05 06:43:24 +0000

    Revert "added comments in seedvariables.js"
    This reverts commit 544af293da2f3f487a2408e37819409c91b94e23.

--- db/seedVariables.js
+++ db/seedVariables.js
@@ -1,16 +1,9 @@
-// Variables for the fake data generation. Alter here, then
-// recreate tables before running seed function again
-
 module.exports = {
   numOfUsers: 200,
   numOfMaps: 50,
   maxNumPinsPerMap: 10,
   maxNumOfFavMapIdsPerUser: 5,
  };
-// Try to stay roughly with the equation below:
 
-// numOfMaps = numOfusers /4
-// maxNumPinsPerMap = numOfMaps / 5
-// maxNumOfFavMapIdsPerUser = maxNumPinsPerMap / 2
-
-
+// Variables for the fake data generation. Alter here, then
+// recreate tables before running seed function again
