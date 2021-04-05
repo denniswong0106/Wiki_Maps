a658c5d62439254f3938c7f9ae77e323cc3a2231
David Radvan <david.radvan@gmail.com>
2021-04-04 17:45:46 -0700

    add mapsShow stylesheet and adjust profile colors

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use("/users", usersRouter);
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("maps_show");
+  res.render("index");
 });
 
 app.listen(PORT, () => {
