544af293da2f3f487a2408e37819409c91b94e23
Dennis Wong <denniswong0106@gmail.com>
2021-04-05 01:13:15 +0000

    added comments in seedvariables.js

--- db/seedVariables.js
+++ db/seedVariables.js
@@ -1,9 +1,16 @@
+// Variables for the fake data generation. Alter here, then
+// recreate tables before running seed function again
+
 module.exports = {
   numOfUsers: 200,
   numOfMaps: 50,
   maxNumPinsPerMap: 10,
   maxNumOfFavMapIdsPerUser: 5,
  };
+// Try to stay roughly with the equation below:
 
-// Variables for the fake data generation. Alter here, then
-// recreate tables before running seed function again
+// numOfMaps = numOfusers /4
+// maxNumPinsPerMap = numOfMaps / 5
+// maxNumOfFavMapIdsPerUser = maxNumPinsPerMap / 2
+
+
