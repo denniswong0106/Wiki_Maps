196fa0982409db95db080e3e93e70698c9fe52d5
Dennis Wong <denniswong0106@gmail.com>
2021-04-08 00:01:51 +0000

    added small changes to handled db connection if not heroku

--- knexfile.js
+++ knexfile.js
@@ -1,11 +1,26 @@
 require('dotenv').config();
 
+let connectionInfo = {};
+
+if (process.env.DATABASE_URL) {
+  connectionInfo = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  }
+} else {
+  connectionInfo = {
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+  }
+};
+
 module.exports = {
   development: {
     client: "postgresql",
-    connection: {
-      connectionString = process.env.DATABASE_URL
-    },
+    connection: connectionInfo,
     migrations: {
       directory: __dirname + "/db/migrations",
     },
